8978d6e5cfadd943b0c84ab64a799532800f1a52
Karthik A <karthikthee7@gmail.com>
2022-03-24 20:55:53 +0530

    Initial commit

---
+++ practice-food-order-app/src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import mealsImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table full fo delicious food!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
