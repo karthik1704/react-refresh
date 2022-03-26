54cceb05ec136b5a7f14644f538ada19990d5184
Karthik A <karthikthee7@gmail.com>
2022-03-26 20:17:40 +0530

    complete

--- class-based-cmp/src/App.js
+++ class-based-cmp/src/App.js
@@ -1,11 +1,26 @@
-import Users from './components/Users';
+import React, { Component } from "react";
+import UserFinder from "./components/UserFinder";
+import UsersContext from "./store/users-context";
 
-function App() {
-  return (
-    <div>
-      <Users />
-    </div>
-  );
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class App extends Component {
+
+  userContext = {
+    users: DUMMY_USERS
+  }
+
+  render() {
+    return (
+      <UsersContext.Provider value={this.userContext}>
+        <UserFinder />
+      </UsersContext.Provider>
+    );
+  }
 }
 
 export default App;
