9bdf6723daff93e581444b3e1f3e01e1ab5bba95
Karthik A <karthikthee7@gmail.com>
2022-03-31 12:13:34 +0530

    complete!

---
+++ diving-into-redux/src/store/index.js
@@ -0,0 +1,51 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import counterReducer from "./counterSlice";
+
+export const INCREMENT = "increment";
+
+
+
+
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+
+
+export default store;
