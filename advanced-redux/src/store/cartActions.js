77a4f51a7331d702597cedca49aaf1b8ceda4dcb
Karthik A <karthikthee7@gmail.com>
2022-04-01 19:44:43 +0530

    complete!.

---
+++ advanced-redux/src/store/cartActions.js
@@ -0,0 +1,85 @@
+import { cartActions } from "./cartSlice";
+import { uiActions } from "./uiSlice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const res = await fetch(
+        "https://react-http-f1bc7-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!res.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+      const data = await res.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (err) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: err.message,
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending!",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://react-http-f1bc7-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (err) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: err.message,
+        })
+      );
+    }
+  };
+};
