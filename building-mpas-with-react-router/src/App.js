80c62e040d2b5594461b1e25b8f348e3548b81bb
Karthik A <karthikthee7@gmail.com>
2022-04-03 19:15:35 +0530

    Initial commit

--- building-mpas-with-react-router/src/App.js
+++ building-mpas-with-react-router/src/App.js
@@ -1,31 +1,11 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
+import { Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+      <Switch>
+        <Route></Route>
+      </Switch>
     </div>
   );
 }
